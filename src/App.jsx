f9763b10059e9860d1c9ec91085bd5535a11122e
flaviare1s <flaviarster@gmail.com>
2024-08-04 11:50:06 -0300

    Imagens movidas para public

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export function App() {
     <>
       <main className={mode === 'light' ? 'bg-bg-mobile-light h-[200px] tablet:bg-bg-desktop-light bg-no-repeat bg-cover md:h-[300px] w-screen' : 'bg-bg-mobile-dark h-[200px] tablet:bg-bg-desktop-dark bg-no-repeat bg-cover md:h-[300px] w-screen'} >
         <header className='flex justify-between items-center px-[26px] pt-12 md:pt-[70px] md:w-[540px] m-auto'>
-          <h1 className='text-white uppercase font-bold tracking-[5px] md:tracking-[15px] text-3xl md:text-4xl'>Todo</h1>
+          <h1 className='text-white uppercase font-bold tracking-[10px] md:tracking-[15px] text-3xl md:text-4xl'>Todo</h1>
           <button onClick={changeMode}><img className='h-5 md:h-[26px]' src={mode === 'light'? darkMode : lightMode } alt="Icon" /></button>
         </header>
         
