3b28b95f6f282d837b37d11ec40600fb4271531d
flaviare1s <flaviarster@gmail.com>
2024-08-04 13:44:52 -0300

    Corrigi estilos

--- src/assets/components/NewTodo.jsx
+++ src/assets/components/NewTodo.jsx
@@ -1,7 +1,7 @@
 export const NewTodo = () => {
   return (
     <form className="relative">
-      <input className="h-12 w-full mt-10 rounded-[5px] px-14 placeholder:text-dark-grayish-blue placeholder:text-xs" type="text" placeholder="Create a new todo..." />
+      <input className="font-josefin-sans text-very-dark-grayish-blue text-xs h-12 w-full mt-10 rounded-[5px] px-14 placeholder:text-dark-grayish-blue placeholder:text-xs" type="text" placeholder="Create a new todo..." />
       <span className="h-5 w-5 border border-light-grayish-blue rounded-full absolute top-[52px] left-[20px]"></span>
     </form>
   )
