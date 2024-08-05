04c4350ac879f36cec7a5265df125bd5c8f70c1a
flaviare1s <flaviarster@gmail.com>
2024-08-04 21:01:35 -0300

    Correção de erros

--- src/assets/components/Todos.jsx
+++ src/assets/components/Todos.jsx
@@ -8,11 +8,11 @@ export const Todos = ({ mode }) => {
     <>
       <section className={`rounded-[5px] mt-[16px] shadow-sm ${mode === 'light' ? 'bg-white' : 'bg-dark-mode-input'}`}>
         {todos.map((todo) => (
-          <div key={todo.id} className={`px-5 h-12 md:h-16 flex justify-between items-center border-b ${mode === 'light' ? 'border-light-grayish-blue' : 'border-very-dark-grayish-blue'}`}>
+          <div key={todo.id} className={`px-5 p-4 md:p-5 flex justify-between items-center border-b ${mode === 'light' ? 'border-light-grayish-blue' : 'border-very-dark-grayish-blue'}`}>
             <div className='flex items-center'>
-              <button className={`h-5 w-5 border ${mode === 'light' ? 'border-light-grayish-blue' : 'border-very-dark-grayish-blue'} rounded-full relative ${todo.completed ? 'bg-gradient-to-r from-linear-gradient-1 to-linear-gradient-2' : ''}`}></button>
+              <button className={`h-5 w-5 border ${mode === 'light' ? 'border-light-grayish-blue' : 'border-very-dark-grayish-blue'} rounded-full relative ${todo.status === 'completed' ? 'bg-gradient-to-r from-linear-gradient-1 to-linear-gradient-2' : ''}`}></button>
               {todo.completed && <img className='absolute p-1 pointer-events-none' src={checkIcon} alt="check" />}
-              <button className={`px-3 text-xs md:text-base text-very-dark-grayish-blue ${todo.completed ? 'text-very-light-grayish-blue line-through' : ''}`}>{todo.title}</button>
+              <button className={`px-3 text-xs md:text-base text-very-dark-grayish-blue ${todo.status === 'completed' ? 'text-very-light-grayish-blue line-through' : ''}`}>{todo.title}</button>
             </div>
             <button><img className='h-3' src={crossIcon} alt="cross" /></button>
           </div>
