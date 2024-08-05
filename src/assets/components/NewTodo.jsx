f887a6dbe8d9cb535da8ee8babe41c1e61777df2
flaviare1s <flaviarster@gmail.com>
2024-08-05 01:22:30 -0300

    Correção de erro

--- src/assets/components/NewTodo.jsx
+++ src/assets/components/NewTodo.jsx
@@ -21,7 +21,7 @@ export const NewTodo = ({ mode, fetchTodos }) => {
 
   return (
     <form onSubmit={handleSubmit} className="relative">
-      <input onChange={(e) => setTitle(e.target.value)} className={`font-josefin-sans text-very-dark-grayish-blue text-xs md:text-base h-12 md:h-16 w-full mt-10 rounded-[5px] px-[52px] placeholder:text-dark-grayish-blue placeholder:text-xs md:placeholder:text-base ${mode === 'light' ? 'bg-white' : 'bg-dark-mode-input'}`} type="text" placeholder="Create a new todo..." />
+      <input value={title} onChange={(e) => setTitle(e.target.value)} className={`font-josefin-sans text-very-dark-grayish-blue text-xs md:text-base h-12 md:h-16 w-full mt-10 rounded-[5px] px-[52px] placeholder:text-dark-grayish-blue placeholder:text-xs md:placeholder:text-base ${mode === 'light' ? 'bg-white' : 'bg-dark-mode-input'}`} type="text" placeholder="Create a new todo..." />
       <span className={`h-5 w-5 border rounded-full absolute top-[52px] md:top-[60px] left-[20px] ${mode === 'light' ? 'border-light-grayish-blue' : 'border-very-dark-grayish-blue'}`}></span>
     </form>
   )
