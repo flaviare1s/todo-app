dc469f4bbd1357eb4f09ecbddceb28dc9283d144
flaviare1s <flaviarster@gmail.com>
2026-01-17 07:45:42 -0300

    Ajusta docker e build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-app/',
-  // build: {
-  //   outDir: 'build',
-  // }
+  base: "/"
 })
